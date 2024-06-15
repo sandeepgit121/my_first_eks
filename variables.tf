fc392c5cb992c8cdfca7283cced956c8b464be62
sandeepgit121 <100903758+sandeepgit121@users.noreply.github.com>
2024-06-15 13:31:37 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "location" {
+    default = "ap-south-1"
+}
+
+variable "os_name" {
+    default = "ami-0e1d06225679bc1c5"
+}
+
+variable "key" {
+    default = "eks"
+}
+
+variable "instance-type" {
+    default = "t2.small"
+}
+
+variable "vpc-cidr" {
+    default = "10.10.0.0/16"  
+}
+
+variable "subnet1-cidr" {
+    default = "10.10.1.0/24"
+  
+}
+variable "subnet2-cidr" {
+    default = "10.10.2.0/24"
+  
+}
+variable "subent-1_az" {
+    default =  "ap-south-1a"  
+}
+variable "subent-2_az" {
+    default =  "ap-south-1b"  
+}
