cde653cb67466a9b0242ced7dd8601d490223297
sandeepgit121 <100903758+sandeepgit121@users.noreply.github.com>
2024-06-17 21:57:49 +0530

    Update eks.tf

--- eks/eks.tf
+++ eks/eks.tf
@@ -244,7 +244,6 @@ resource "aws_eks_node_group" "backend" {
   instance_types = ["t2.small"]
   remote_access {
     ec2_ssh_key = "eks"
-    source_security_group_ids = [var.sg_ids]
   } 
   
   labels =  tomap({env = "dev"})
