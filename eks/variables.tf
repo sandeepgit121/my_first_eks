687978e6fc6e131b4ad30d04673dff90fd59fc65
sandeepgit121 <100903758+sandeepgit121@users.noreply.github.com>
2024-06-17 22:05:50 +0530

    Update variables.tf

--- eks/variables.tf
+++ eks/variables.tf
@@ -18,16 +18,13 @@ variable "vpc-cidr" {
     default = "10.10.0.0/16"  
 }
 
-variable "subnet1-cidr" {
+variable "subnet-cidr" {
     default = "10.10.1.0/24"
   
 }
-variable "subnet2-cidr" {
+variable "subnet-cidr" {
     default = "10.10.2.0/24"
 }
-variable "subent-1_az" {
+variable "subent_az" {
     default =  "ap-south-1a"  
-}
-variable "subent-2_az" {
-    default =  "ap-south-1b"  
 }
