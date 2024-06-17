175d1076fabace2ac440fe31dbd9285700cf3945
sandeepgit121 <100903758+sandeepgit121@users.noreply.github.com>
2024-06-17 22:03:27 +0530

    Update eks.tf

--- eks/eks.tf
+++ eks/eks.tf
@@ -1,122 +1,15 @@
-resource "aws_iam_role" "master" {
-  name = "eks-master"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
+provider "aws" {
+  region = var.location
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.master.name
+resource "aws_instance" "demo-server" {
+ ami = var.os_name
+ key_name = var.key 
+ instance_type  = var.instance-type
+ /*associate_public_ip_address = true*/
+subnet_id = aws_subnet.demo_subnet.id
+vpc_security_group_ids = [aws_security_group.demo-vpc-sg.id]
 }
-
-resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.master.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.master.name
-}
-
-resource "aws_iam_role" "worker" {
-  name = "ed-eks-worker"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_policy" "autoscaler" {
-  name   = "ed-eks-autoscaler-policy"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "autoscaling:DescribeAutoScalingGroups",
-        "autoscaling:DescribeAutoScalingInstances",
-        "autoscaling:DescribeTags",
-        "autoscaling:DescribeLaunchConfigurations",
-        "autoscaling:SetDesiredCapacity",
-        "autoscaling:TerminateInstanceInAutoScalingGroup",
-        "ec2:DescribeLaunchTemplateVersions"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.worker.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.worker.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonSSMManagedInstanceCore" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  role       = aws_iam_role.worker.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.worker.name
-}
-
-resource "aws_iam_role_policy_attachment" "x-ray" {
-  policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
-  role       = aws_iam_role.worker.name
-}
-resource "aws_iam_role_policy_attachment" "s3" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-  role       = aws_iam_role.worker.name
-}
-
-resource "aws_iam_role_policy_attachment" "autoscaler" {
-  policy_arn = aws_iam_policy.autoscaler.arn
-  role       = aws_iam_role.worker.name
-}
-
-resource "aws_iam_instance_profile" "worker" {
-  depends_on = [aws_iam_role.worker]
-  name       = "ed-eks-worker-new-profile"
-  role       = aws_iam_role.worker.name
-}
-
-###############################################################################################################
 
 // Create VPC
 resource "aws_vpc" "demo-vpc" {
@@ -124,24 +17,13 @@ resource "aws_vpc" "demo-vpc" {
 }
 
 // Create Subnet
-resource "aws_subnet" "demo_subnet-1" {
+resource "aws_subnet" "demo_subnet" {
   vpc_id     = aws_vpc.demo-vpc.id 
   cidr_block = var.subnet1-cidr
-  availability_zone = var.subent-1_az
-  map_public_ip_on_launch = "true"
+  availability_zone = var.subent_az
 
   tags = {
-    Name = "demo_subnet-1"
-  }
-}
-  resource "aws_subnet" "demo_subnet-2" {
-  vpc_id     = aws_vpc.demo-vpc.id 
-  cidr_block = var.subnet2-cidr
-  availability_zone = var.subent-2_az
-  map_public_ip_on_launch = "true"
-
-  tags = {
-    Name = "demo_subnet-2"
+    Name = "demo_subnet"
   }
 }
 
@@ -168,13 +50,8 @@ resource "aws_route_table" "demo-rt" {
 }
 
 // associate subnet with route table 
-resource "aws_route_table_association" "demo-rt_association-1" {
-  subnet_id      = aws_subnet.demo_subnet-1.id 
-
-  route_table_id = aws_route_table.demo-rt.id
-}
-resource "aws_route_table_association" "demo-rt_association-2" {
-  subnet_id      = aws_subnet.demo_subnet-2.id 
+resource "aws_route_table_association" "demo-rt_association" {
+  subnet_id      = aws_subnet.demo_subnet.id 
 
   route_table_id = aws_route_table.demo-rt.id
 }
@@ -184,26 +61,14 @@ resource "aws_security_group" "demo-vpc-sg" {
   name        = "demo-vpc-sg"
  
   vpc_id      = aws_vpc.demo-vpc.id
-}
-
-resource "aws_security_group" "worker_node_sg" {
-  name        = "eks-test"
-  description = "Allow ssh inbound traffic"
-  vpc_id      =  var.vpc_id
 
   ingress {
-    description      = "ssh access to public"
+
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-  }
-ingress {
-    description      = "ssh access to public"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
@@ -211,59 +76,12 @@ ingress {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
+  tags = {
+    Name = "allow_tls"
+  }
 }
-resource "aws_eks_cluster" "eks" {
-  name = "ed-eks-01"
-  role_arn = aws_iam_role.master.arn
 
-  vpc_config {
-    subnet_ids = [var.subnet_ids[0],var.subnet_ids[1]]
-  }
-  
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.AmazonEKSServicePolicy,
-    aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
-    aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
-    #aws_subnet.pub_sub1,
-    #aws_subnet.pub_sub2,
-  ]
-
-}
-#################################################################################################################
-
-resource "aws_eks_node_group" "backend" {
-  cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "dev"
-  node_role_arn   = aws_iam_role.worker.arn
-  subnet_ids = [var.subnet_ids[0],var.subnet_ids[1]]
-  capacity_type = "ON_DEMAND"
-  disk_size = "20"
-  instance_types = ["t2.small"]
-  remote_access {
-    ec2_ssh_key = "eks"
-  } 
-  
-  labels =  tomap({env = "dev"})
-  
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
-  }
-
-  update_config {
-    max_unavailable = 1
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-    #aws_subnet.pub_sub1,
-    #aws_subnet.pub_sub2,
-  ]
-}
 
