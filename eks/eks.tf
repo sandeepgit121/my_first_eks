e75db9a4e5608b11a6458f8d8c23d118a5b6904f
sandeepgit121 <100903758+sandeepgit121@users.noreply.github.com>
2024-06-17 21:45:30 +0530

    Update eks.tf

--- eks/eks.tf
+++ eks/eks.tf
@@ -185,21 +185,24 @@ resource "aws_security_group" "demo-vpc-sg" {
  
   vpc_id      = aws_vpc.demo-vpc.id
 
-  ingress {
+resource "aws_security_group" "worker_node_sg" {
+  name        = "eks-test"
+  description = "Allow ssh inbound traffic"
+  vpc_id      =  var.vpc_id
 
+  ingress {
+    description      = "ssh access to public"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 ingress {
-
+    description      = "ssh access to public"
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
@@ -207,12 +210,8 @@ ingress {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
-  tags = {
-    Name = "allow_tls"
-  }
 }
 resource "aws_eks_cluster" "eks" {
   name = "ed-eks-01"
