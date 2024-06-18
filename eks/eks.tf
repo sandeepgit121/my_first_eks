789dd22a5ab66113987c03975892dc45dfc3c953
sandeepgit121 <100903758+sandeepgit121@users.noreply.github.com>
2024-06-18 12:32:22 +0530

    Update eks.tf

--- eks/eks.tf
+++ eks/eks.tf
@@ -226,8 +226,8 @@ resource "aws_eks_node_group" "backend" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "dev"
   node_role_arn   = aws_iam_role.worker.arn
-  
-  subnet_ids = [aws_subnet.demo_subnet-1.id,aws_subnet.demo_subnet-2.id]
+  map_public_ip_on_launch = true
+  /*subnet_ids = [aws_subnet.demo_subnet-1.id,aws_subnet.demo_subnet-2.id]*/
   capacity_type = "ON_DEMAND"
   disk_size = "20"
   instance_types = ["t2.small"]
