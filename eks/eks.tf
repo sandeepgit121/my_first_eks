35fb906297dfffa9d2404fb43404d1c17f0dc967
sandeepgit121 <sandeep.msinternational@gmail.com>
2024-06-18 20:26:36 +0530

    1

--- eks/eks.tf
+++ eks/eks.tf
@@ -238,7 +238,7 @@ vpc_id = aws_vpc.demo-vpc.id
 }
 module "eks" {
 source "./eks"
-sg_ids=module.sgs.security_group_public
+sg_ids = module.sgs.security_group_public
 vpc_id = aws_vpc.demo-vpc.id
 subnet_ids = [aws_subnet.demo_subnet-1.id,aws_subnet.demo_subnet-2.id]
 
