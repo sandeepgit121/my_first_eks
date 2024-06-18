7fa26a4c248cde9e3d528baa7d0528091a0564e8
sandeepgit121 <100903758+sandeepgit121@users.noreply.github.com>
2024-06-18 11:06:08 +0530

    Update eks.tf

--- eks/eks.tf
+++ eks/eks.tf
@@ -214,7 +214,7 @@ resource "aws_eks_cluster" "eks" {
   role_arn = aws_iam_role.master.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.demo_subnet-1.id],[aws_subnet.demo_subnet-2.id]
+    subnet_ids = [aws_subnet.demo_subnet-1.id][aws_subnet.demo_subnet-2.id]
   }
   
   depends_on = [
@@ -233,7 +233,7 @@ resource "aws_eks_node_group" "backend" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "dev"
   node_role_arn   = aws_iam_role.worker.arn
-  subnet_ids = [aws_subnet.demo_subnet-1.id],[aws_subnet.demo_subnet-2.id]
+  subnet_ids = [aws_subnet.demo_subnet-1.id][aws_subnet.demo_subnet-2.id]
   capacity_type = "ON_DEMAND"
   disk_size = "20"
   instance_types = ["t2.small"]
