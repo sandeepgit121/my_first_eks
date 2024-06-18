43fd6e390a8a232cae2b55c865164dc7e7f092c1
sandeepgit121 <sandeep.msinternational@gmail.com>
2024-06-18 18:47:34 +0530

    ekschange

--- eks/eksvariables.tf
+++ eks/eksvariables.tf
@@ -18,16 +18,11 @@ variable "vpc-cidr" {
     default = "10.10.0.0/16"  
 }
 
-variable "subnet-1-cidr" {
+variable "subnet-cidr" {
     default = "10.10.1.0/24"
 
-variable "subnet-2-cidr" {
-    default = "10.10.2.0/24"
   
 }
-variable "subent-1_az" {
-    default =  "ap-south-1a"  
-}
-variable "subent-2_az" {
+variable "subent_az" {
     default =  "ap-south-1a"  
 }
