19cbe349c08c730c0615e1466bac81139b1d9ecd
sandeepgit121 <sandeep.msinternational@gmail.com>
2024-06-18 19:14:41 +0530

    7

--- eks/eks.tf
+++ eks/eks.tf
@@ -57,43 +57,6 @@ resource "aws_route_table_association" "demo-rt_association-2" {
   route_table_id = aws_route_table.demo-rt.id
 }
 
-// create a security group 
-
-resource "aws_security_group" "demo-vpc-sg" {
-  name        = "demo-vpc-sg"
- 
-  vpc_id      = aws_vpc.demo-vpc.id
-
-  ingress {
-
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-  ingress {
-
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
-
 
 resource "aws_iam_role" "master" {
   name = "ed-eks-master"
