f6be1d2d7b97ed70998f2d32e56d05c06226345f
sandeepgit121 <100903758+sandeepgit121@users.noreply.github.com>
2024-06-18 11:10:24 +0530

    Update eks.tf

--- eks/eks.tf
+++ eks/eks.tf
@@ -214,7 +214,7 @@ resource "aws_eks_cluster" "eks" {
   role_arn = aws_iam_role.master.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.demo_subnet-1.id][aws_subnet.demo_subnet-2.id]
+    subnet_ids = [aws_subnet.demo_subnet-1.id[0]][aws_subnet.demo_subnet-2.id[1]]
   }
   
   depends_on = [
@@ -233,13 +233,13 @@ resource "aws_eks_node_group" "backend" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "dev"
   node_role_arn   = aws_iam_role.worker.arn
-  subnet_ids = [aws_subnet.demo_subnet-1.id][aws_subnet.demo_subnet-2.id]
+  subnet_ids = [aws_subnet.demo_subnet-1.id[0]][aws_subnet.demo_subnet-2.id[1]]
   capacity_type = "ON_DEMAND"
   disk_size = "20"
   instance_types = ["t2.small"]
   remote_access {
     ec2_ssh_key = "eks"
-    source_security_group_ids = [var.sg_ids]
+    source_security_group_ids = aws_security_group.demo-vpc-sg.id
   } 
   
   labels =  tomap({env = "dev"})
