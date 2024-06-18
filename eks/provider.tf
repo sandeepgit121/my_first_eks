3e205c2baff86ec281bcdefd4d4289a26e0941e5
sandeepgit121 <100903758+sandeepgit121@users.noreply.github.com>
2024-06-18 14:50:26 +0530

    Update provider.tf

--- eks/provider.tf
+++ eks/provider.tf
@@ -1,16 +1,16 @@
 locals {
-  region = "us-east-1"
+  region = "ap-south-1"
   name   = "amonkincloud-cluster"
-  vpc_cidr = "10.123.0.0/16"
+  vpc_cidr = "10.10.0.0/16"
   azs      = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+  public_subnets  = ["10.10.1.0/24", "10.10.2.0/24"]
+  private_subnets = ["10.10.3.0/24", "10.10.4.0/24"]
+  intra_subnets   = ["10.10.5.0/24", "10.10.6.0/24"]
   tags = {
     Example = local.name
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
