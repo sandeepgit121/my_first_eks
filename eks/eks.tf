27aea15bd8df036e68a07d40e797fb387854b848
sandeepgit121 <100903758+sandeepgit121@users.noreply.github.com>
2024-06-19 12:11:14 +0530

    Update eks.tf

--- eks/eks.tf
+++ eks/eks.tf
@@ -262,15 +262,4 @@ resource "aws_eks_node_group" "backend" {
   ]
 }
 
-module "sgs" {
-source = "./sg_eks"
-vpc_id = aws_vpc.demo-vpc.id
 
-}
-module "eks" {
-source = "./eks"
-sg_ids = module.sgs.security_group_public
-vpc_id = aws_vpc.demo-vpc.id
-subnet_ids = [aws_subnet.demo_subnet-1.id,aws_subnet.demo_subnet-2.id]
-
-}
