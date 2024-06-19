bca92be43ea1fdd481e87bc392d0505e8d8376e4
sandeepgit121 <100903758+sandeepgit121@users.noreply.github.com>
2024-06-19 12:06:31 +0530

    Update eks.tf

--- eks/eks.tf
+++ eks/eks.tf
@@ -57,34 +57,7 @@ resource "aws_route_table_association" "demo-rt_association-2" {
   route_table_id = aws_route_table.demo-rt.id
 }
 
-resource "aws_security_group" "worker_node_sg" {
-  name        = "eks-test"
-  description = "Allow ssh inbound traffic"
-  vpc_id      =  aws_vpc.demo-vpc.id
 
-  ingress {
-    description      = "ssh access to public"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-ingress {
-    description      = "ssh access to public"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-}
 
 
 resource "aws_iam_role" "master" {
