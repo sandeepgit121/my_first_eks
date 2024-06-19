99a5cc97eeb28cae56046722f72d8732a878fc3c
sandeepgit121 <100903758+sandeepgit121@users.noreply.github.com>
2024-06-19 12:06:46 +0530

    Update sg.tf

--- eks/sg_eks/sg.tf
+++ eks/sg_eks/sg.tf
@@ -1,1 +1,28 @@
+resource "aws_security_group" "worker_node_sg" {
+  name        = "eks-test"
+  description = "Allow ssh inbound traffic"
+  vpc_id      =  aws_vpc.demo-vpc.id
 
+  ingress {
+    description      = "ssh access to public"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+ingress {
+    description      = "ssh access to public"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+}
